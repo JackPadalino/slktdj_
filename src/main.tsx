abf0112626dccac90a4331d235ff5653c29b5f75
JackPadalino <pada0867@gmail.com>
2024-03-24 15:54:35 -0400

    removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client";
 // import App from "./App.tsx";
 import Live from "./Live.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Live />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<Live />);
